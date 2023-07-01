dbdb50e9b5b1a4806735c3ab41663d8a4ff8bbf6
loukya-7 <131159563+loukya-7@users.noreply.github.com>
2023-07-01 15:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+async function showMessageAfterDelay() {
+  const textInput = document.getElementById('text');
+  const delayInput = document.getElementById('delay');
+  const outputDiv = document.getElementById('output');
+
+  const text = textInput.value;
+  const delay = parseInt(delayInput.value);
+
+  // Wait for the specified delay
+  await new Promise(resolve => setTimeout(resolve, delay));
+
+  // Display the message on the webpage
+  outputDiv.textContent = text;
+}
+
